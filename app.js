949f07c93ee84c13171103fb696b0703d575dfd1
Vu Tran <vutran1412@icloud.com>
2019-07-20 11:58:09 -0700

    set up the initial UI controller

--- app.js
+++ app.js
@@ -3,13 +3,47 @@ var budgetController = (function() {
 })()
 
 var UIController = (function() {
-    // Display the Income and Expenses
+
+    let DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return  {
+        getInput: function() {
+            return ({
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            })
+        },
+        getDOMStrings: function() {
+            return DOMStrings
+        }
+    }
+
 })()
 
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', () => {
-        // Get the field input data
 
+    var setUpEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings()
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+        document.addEventListener('keypress', (event) => {
+            // if keycode is enter then get the field input data
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem()
+            }
+        })
+    }
+
+
+    var ctrlAddItem = function() {
+        // Get the field input data
+        let budgetItem = UICtrl.getInput()
         // Add the item to the budget controller
 
         // Add the item to the UI
@@ -17,22 +51,19 @@ var controller = (function(budgetCtrl, UICtrl) {
         // Calcultate the budget
 
         // Display the budget to the UI
-    })
+    }
 
+    
+    return {
+        init: function() {
+            setUpEventListeners()
+            console.log('App set up!')
+        }
+    }
 
-    document.addEventListener('keypress', (event) => {
-        // if keycode is enter then get the field input data
-
-        // Add the item to teh budget controller
-
-        // add the item to the UI
-
-        // Calculate the budget
-
-        // Display the budget to the UI
-    })
+    
 
 
 })(budgetController, UIController)
 
-
+controller.init()
